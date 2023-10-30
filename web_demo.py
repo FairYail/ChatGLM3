499891202a78ef9ab773ea9bba8687be62eecc24
“zhuchen” <zhuchen@hortorgames.com>
2023-10-30 16:08:38 +0800

    feat:修改启动接口

--- web_demo.py
+++ web_demo.py
@@ -105,4 +105,5 @@ with gr.Blocks() as demo:
 
     emptyBtn.click(reset_state, outputs=[chatbot, history, past_key_values], show_progress=True)
 
-demo.queue().launch(share=False, inbrowser=True)
+# demo.queue().launch(share=False, inbrowser=True)
+demo.queue().launch(share=False, inbrowser=True, server_name="0.0.0.0", server_port=19396)
